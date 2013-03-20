88c4c8a99376f9752877ddf6baba4a4d99297743
Kalle Blomström <kalle.blomstrom@gmail.com>
2013-03-20 16:47:48 +0100

    Removes event edit dialog after button press

--- src/com/blmstrm/controller/MainController.java
+++ src/com/blmstrm/controller/MainController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,54 +11,54 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.blmstrm.db.MyEventRepository;
 import com.blmstrm.model.MyEvent;
+import com.mongodb.util.JSON;
 
 @Controller
 public class MainController{
 
-  @Autowired
-  MyEventRepository eventRepository;
+	@Autowired
+	MyEventRepository eventRepository;
 
-  /*Return calendar view*/
-  @RequestMapping(value="/",  method = RequestMethod.GET)
-    public ModelAndView index(){
-      return new ModelAndView("index");
-    }
-
-  /*Add event to database through HTTP.POST*/
-  @RequestMapping(value="/events",  method = RequestMethod.POST)
-    @ResponseBody
-    public MyEvent addEvent(@RequestBody MyEvent newEvent){
-      eventRepository.addEvent(newEvent);
-      return newEvent;
-    }
-
-  /*Update event in database through HTTP.PUT*/
-  @RequestMapping(value="/events/{id}",  method = RequestMethod.PUT)
-    @ResponseBody
-    public MyEvent updateEvent(@PathVariable String id, @RequestBody MyEvent updatedEvent){
-      eventRepository.updateEvent(updatedEvent);
-      return updatedEvent;
-    }
-
-  /*Update event in database through HTTP.DELETE*/
-  @RequestMapping(value="/events/{id}",  method = RequestMethod.DELETE)
-  @ResponseBody
-     public MyEvent removeEvent(@PathVariable String id){
-	  eventRepository.removeEvent(id);
-	  return new MyEvent();
+	/*Return calendar view*/
+	@RequestMapping(value="/",  method = RequestMethod.GET)
+	public ModelAndView index(){
+		return new ModelAndView("index");
 	}
 
-  /*Fetch all events from database*/
-  @RequestMapping(value="/events", method = RequestMethod.GET)
-    @ResponseBody
-    public List<MyEvent> getAllEvents(){
-      return eventRepository.getAllEvents();
-    }
+	/*Add event to database through HTTP.POST*/
+	@RequestMapping(value="/events",  method = RequestMethod.POST)
+	@ResponseBody
+	public MyEvent addEvent(@RequestBody MyEvent newEvent){
+		eventRepository.addEvent(newEvent);
+		return newEvent;
+	}
+
+	/*Update event in database through HTTP.PUT*/
+	@RequestMapping(value="/events/{id}",  method = RequestMethod.PUT)
+	@ResponseBody
+	public MyEvent updateEvent(@PathVariable String id, @RequestBody MyEvent updatedEvent){
+		eventRepository.updateEvent(updatedEvent);
+		return updatedEvent;
+	}
+
+	/*Update event in database through HTTP.DELETE*/
+	@RequestMapping(value="/events/{id}",  method = RequestMethod.DELETE)
+	@ResponseBody
+	public MyEvent removeEvent(@PathVariable String id){
+		eventRepository.removeEvent(id);
+		return new MyEvent();
+	}
+
+	/*Fetch all events from database*/
+	@RequestMapping(value="/events", method = RequestMethod.GET)
+	@ResponseBody
+	public List<MyEvent> getAllEvents(){
+		return eventRepository.getAllEvents();
+	}
 
 }
 
